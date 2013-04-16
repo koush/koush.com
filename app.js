5596afaedd0e95f7a9dc7070f19acd2a7b8bfa97
Koushik Dutta <koushd@gmail.com>
2013-04-16 14:10:31 -0700

    log host

--- app.js
+++ app.js
@@ -71,6 +71,7 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(function(req, res, next) {
+    console.log(req.headers.host);
     if ('www.koush.com' == req.headers.host) {
       res.redirect('http://koush.com' + req.path);
       return;
