aa2380f8091e5b8768db8cfdf7ab868a8922721c
Koushik Dutta <koushd@gmail.com>
2013-04-16 20:57:10 -0700

    comment out socket.io tests

--- tests.js
+++ tests.js
@@ -1,13 +1,13 @@
 var WebSocketServer = require('websocket').server;
 
 exports.route = function(server, app) {
-  var io = require('socket.io').listen(server);
-  io.sockets.on('connection', function (socket) {
-    socket.on('message', function(msg) {
-      console.log(msg);
-      socket.send(msg);
-    });
-  });
+  // var io = require('socket.io').listen(server);
+  // io.sockets.on('connection', function (socket) {
+  //   socket.on('message', function(msg) {
+  //     console.log(msg);
+  //     socket.send(msg);
+  //   });
+  // });
 
   var wsServer = new WebSocketServer({
       httpServer: server
